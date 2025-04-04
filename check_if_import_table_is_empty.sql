97eaf4f1c07a4bed149846c6735c9e55305fb392
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2025-04-04 17:03:31 -0400

    initial commit

---
+++ check_if_import_table_is_empty.sql
@@ -0,0 +1,4 @@
+SELECT * FROM staging_import LIMIT 20
+
+-- Manually clean 
+-- TRUNCATE TABLE staging_import
---
+++ show_unique_tickers.sql
@@ -0,0 +1,4 @@
+-- Select all unique ticker symbols
+SELECT DISTINCT ticker_symbol
+FROM stocks
+ORDER BY ticker_symbol
